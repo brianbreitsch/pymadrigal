e2dc756259e6a0a0826ce9ee4f7ba279b17c23f1
brianbreitsch <brianbreitsch@gmail.com>
2015-07-26 10:31:42 -0500

    notebooks updated

--- madrigal/__init__.py
+++ madrigal/__init__.py
@@ -1,3 +1,3 @@
 # ini for pymadrigal
-from pymadrigal.parse import parse_madrigal_hdf5, create_2d_image, get_madrigal_column_names, get_record_altitudes, get_record_datetimes
+from madrigal.parse import parse_madrigal_hdf5, create_2d_image, get_madrigal_column_names, get_record_altitudes, get_record_datetimes
 
